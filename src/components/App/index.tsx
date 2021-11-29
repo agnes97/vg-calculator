27759c3b111b6065e700f980befe442eb7f2a17d
Jana Chaloupková <janachaloupkova97@gmail.com>
2021-11-29 17:43:42 +0100

    ✨ Add Calculator buttons and fill them with values

---
+++ src/components/App/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './index.css';
+import Calculator from '../Calculator';
+
+const App: React.FC = () => (
+  <main className="page-wrapper">
+    <Calculator />
+  </main>
+)
+
+export default App;
