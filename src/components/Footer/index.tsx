23f0b0b87c970759595fb97e41ea46a464314363
Jana Chaloupková <janachaloupkova97@gmail.com>
2021-11-29 21:23:16 +0100

    ✨ Add Footer Component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css'
+
+const Footer: React.FC = () => (
+    <footer>
+        <p>&#169; 2021 Jana Chaloupková</p>
+    </footer>
+)
+
+export default Footer
